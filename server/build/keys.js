da348726650fb9dd4dca426e4835188ab3d70cf3
Jesus Arredondo <chuyarredondo@@gmail.com>
2019-10-02 22:57:20 -0500

    remove Keys.js

--- server/build/keys.js
+++ server/build/keys.js
@@ -1,10 +1,1 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'creat6ky_english_connect_db'
-    }
-};
